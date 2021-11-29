0cc20dcc792ba7c12d636d1f148ae1f95012be69
VladYoSlav <vaifaer@gmail.com>
2021-11-30 02:05:45 +0300

    Deleted unused popouts

--- src/popouts/index.tsx
+++ src/popouts/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { PopoutRoot } from '@cteamdev/router'
-import { TestAlert } from './TestAlert'
-import { TestActionSheet } from './TestActionSheet'
 import { Loading } from './Loading'
 
 export const Popouts = () => {
   return (
     <PopoutRoot>
-      <TestAlert nav='alert' />
-      <TestActionSheet nav='action-sheet' />
       <Loading nav='loading' />
     </PopoutRoot>
   )
