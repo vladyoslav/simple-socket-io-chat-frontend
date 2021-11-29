2274dc7b23a456cf4defcdb99d8c14ee87b828a4
VladYoSlav <vaifaer@gmail.com>
2021-11-30 01:36:22 +0300

    Removed unused atom

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,6 +8,4 @@ export const snackbarAtom = atom<Snackbar | undefined>(undefined, 'snackbar')
 
 export const messagesAtom = atom<message[]>([], 'messages')
 
-export const connectionErrorAtom = atom<string>('', 'connectionError')
-
 export const nicknameAtom = atom<string>('', 'nickname')
